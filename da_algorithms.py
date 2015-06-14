b409209b5be8b456abb74ddbf1b12b04fcb3fb7f
myuuuuun <mail@myuuuuun.com>
2015-06-14 22:36:15 +0900

    is_stable_matching()　のエラーを修正 選好表をランダムにつくる random_preference_table()  を作成

--- da_algorithms.py
+++ da_algorithms.py
@@ -5,13 +5,12 @@
 from __future__ import division
 import math
 import functools  #for python3
-from random import uniform, normalvariate
+from random import uniform, normalvariate, shuffle
 import numpy as np
 np.set_printoptions(threshold=np.nan)
 
-# INPUT: 申し込み側の選好、受け入れ側の選好
-# OUTPUT: マッチング
 
+# 1対1のケースのGAアルゴリズム
 def gale_shapley(applicant_prefers_input, host_prefers_input):
 
      # NumPy Arrayに変換
@@ -35,9 +34,6 @@ def gale_shapley(applicant_prefers_input, host_prefers_input):
      # {man0: woman3, man1, woman0,...}というマッチングを入れる
      matching = {}
 
-
-     print("探索開始\n")
-
      while len(stack) > 0:
           # スタックから1人応募者を取り出す
           applicant = stack.pop(0)
@@ -45,16 +41,11 @@ def gale_shapley(applicant_prefers_input, host_prefers_input):
           # 取り出した応募者の選好表
           applicant_preference = applicant_preferences[applicant]
 
-          print("Man{0} さんの探索開始".format(applicant))
-
-          for index, host in enumerate(applicant_preference):
-               print("Man{0} さんの 選好順位{1}位は Woman{2}さん".format(applicant, index+1, host))
+          for host in applicant_preference:
                host_preference = host_preferences[host]
 
                # 相手が未マッチングなら
                if host_preference[-1] == False:
-                    print("Woman{0} さんは未マッチング".format(host))
-                    print("Man{0} さんとWoman{1} さん仮マッチング".format(applicant, host))
                     host_preference[-1] = 1
                     applicant_preference[-1] = 1
                     matching[applicant] = host
@@ -64,36 +55,25 @@ def gale_shapley(applicant_prefers_input, host_prefers_input):
                else:
                     # 既にマッチング済みの相手を代入
                     matched = [k for k, v in matching.items() if v == host][0]
-                    print("Woman{0} さんは Man{1} さんとマッチング済".format(host, matched))
 
                     # 新しい応募者と、既にマッチング済みの相手の、受け入れ側における選好順位を比較
                     rank_matched = np.where(host_preference[:-1] == matched)[0][0]
                     rank_applicant = np.where(host_preference[:-1] == applicant)[0][0]
-                    print("Woman{0} さんの選好表におけるMan{1} さんの順位は {2}位, Man{3} さんの順位は {4}位".format(host, applicant, rank_applicant+1, host, rank_matched+1))
-
+                    
                     # もし受け入れ側が新しい応募者の方を好むなら
                     if rank_matched > rank_applicant:
-                         print("Man{0} さんとWoman{1} さんのマッチング解除".format(matched, host))
                          applicant_preference[-1] = 1
                          del matching[matched]
 
-                         print("Man{0} さんとWoman{1} さん仮マッチング".format(applicant, host))
                          matching[applicant] = host
                          applicant_preferences[matched][-1] = 0
                          stack.append(matched)
                          break
 
-                    print("Man{0} さんとWoman{1} さんはマッチングせず……".format(applicant, host))
-
-          print("Man{0} さんの探索終了\n".format(applicant))
-
-     print("\n全探索終了\nマッチングは")
-
-     print(matching)
-
      return matching
 
 
+# Stable Matchingになっているかを調べる
 def is_stable_matching(matching, applicant_prefers_input, host_prefers_input):
 
      # NumPy Arrayに変換
@@ -123,47 +103,52 @@ def is_stable_matching(matching, applicant_prefers_input, host_prefers_input):
 
           for newhost in applicant_preference[:rank_matched]:
                newhost_preference = host_preferences[newhost]
+               newhost_matched_list = [k for k, v in matching.items() if v == newhost]
 
                # 新しいhostが未マッチングなら、applicantはこの人とマッチングした方が良いので、入力はstable matchingでない
-               if newhost_preference[-1] == 0:
+               if len(newhost_matched_list) == 0:
                     return False
 
                # 新しいhostがマッチング済みなら、その人の現在のマッチング相手の順位と自分の順位を比べて、
                # 自分のほうが高ければ、駆け落ちするのが良いので、入力はstable matchingではない
                else:
-                    newhost_matched = [k for k, v in matching.items() if v == newhost][0]
+                    newhost_matched = newhost_matched_list[0]
 
                     rank_applicant = np.where(newhost_preference == applicant)[0][0]
                     rank_newhost_matched = np.where(newhost_preference == newhost_matched)[0][0]
 
                     if rank_applicant < rank_newhost_matched:
+                         print(applicant, matched, newhost, newhost_matched)
                          return False 
 
      return True
 
 
+# 選好表を適当に作る
+def random_preference_table(row, col):
+     output = []
+     li = list(range(col))
+     for i in range(row):
+          li2 = li[:]
+          shuffle(li2)
+          output.append(li2)
 
-applicant_table = np.array(
-     [[0, 1, 2, 3, 4], 
-      [4, 1, 2, 0, 3],
-      [3, 0, 2, 4, 1],
-      [4, 2, 0, 1, 3],
-      [3, 0, 4, 1, 2],
-     ])
+     return output
 
 
-host_table = [[0, 1, 2, 3, 4], 
-     [4, 2, 0, 1, 3],
-     [3, 0, 4, 1, 2],
-     [4, 1, 2, 0, 3],
-     [3, 0, 2, 4, 1],
-    ]
 
-matching = gale_shapley(applicant_table, host_table)
-matching2 = {0: 1, 1: 0, 2: 2, 3: 4, 4: 3}
+for i in range(100):
+     print("\n+-------------------------------------+\n")
+     print("{0}回目".format(i))
+     app_table = random_preference_table(5, 5)
+     hos_table = random_preference_table(5, 5)
+     matching = gale_shapley(app_table, hos_table)
+     is_stable = is_stable_matching(matching, app_table, hos_table)
 
-print(is_stable_matching(matching, applicant_table, host_table))
-print(is_stable_matching(matching2, applicant_table, host_table))
+     print("申し込み側の選好は: {0}".format(app_table))
+     print("受け入れ側の選好は: {0}".format(hos_table))
+     print("マッチングは: {0}".format(matching))
+     print("安定マッチングが得られたか? {0}".format(is_stable))
 
 
 
