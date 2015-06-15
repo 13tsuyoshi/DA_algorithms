907b0e368236bd1257aa9c4b498f47144110c72a
myuuuuun <mail@myuuuuun.com>
2015-06-15 17:06:56 +0900

    マッチングを全出力する関数、安定マッチングを全出力する関数、マッチングのスコアを計算する関数を追加

--- da_algorithms.py
+++ da_algorithms.py
@@ -118,7 +118,7 @@ def is_stable_matching(matching, applicant_prefers_input, host_prefers_input):
                     rank_newhost_matched = np.where(newhost_preference == newhost_matched)[0][0]
 
                     if rank_applicant < rank_newhost_matched:
-                         print(applicant, matched, newhost, newhost_matched)
+                         #print(applicant, matched, newhost, newhost_matched)
                          return False 
 
      return True
@@ -136,19 +136,146 @@ def random_preference_table(row, col):
      return output
 
 
+# 可能なマッチングを全出力する
+def all_matching(size):
+     if size > 7:
+          print("sizeが大きすぎます")
+          return False
 
-for i in range(100):
+     li = list(range(size))
+
+
+     def __matching_list(li):
+          if len(li) == 1:
+               return [li]
+
+          output = []
+          for index, element in enumerate(li):
+               li2 = li[:]
+               li2.pop(index)
+               m = __matching_list(li2)
+               for x in m:
+                    output.append([element] + x)
+
+          return output
+
+
+     def __make_dict(pattern):
+          size = len(li)
+          outdict = {}
+          for i in range(size):
+               outdict[i] = pattern[i]
+          return outdict
+
+     output = []
+
+     for pattern in __matching_list(li):
+          output.append(__make_dict(pattern))
+
+     return output
+
+
+# 可能な安定マッチングを全出力する
+def all_stable_matching(applicant_prefers_input, host_prefers_input):
+     # NumPy Arrayに変換
+     applicant_preferences = np.array(applicant_prefers_input, dtype=int)
+     host_preferences = np.array(host_prefers_input, dtype=int)
+
+     # 選好表の行列数をチェック
+     row, col = applicant_preferences.shape
+     row_host, col_host = host_preferences.shape
+     if row != col_host or col != row_host:
+          exit(-1)
+
+     if row > 7 or col > 7:
+          print("sizeが大きすぎます")
+          return False
+
+     matchings = all_matching(row)
+
+     output = []
+     for matching in matchings:
+          if is_stable_matching(matching, applicant_preferences, host_preferences):
+               output.append(matching)
+
+     return output
+
+
+# マッチングのスコアを、申し込み側 / 受け入れ側それぞれについて求める
+# 一番良い相手とペアになった時のスコアが(相手側の人数)ポイント、一番悪い相手とペアになった時のスコアが0ポイント
+def matching_score(matching, applicant_prefers_input, host_prefers_input):
+     # NumPy Arrayに変換
+     applicant_preferences = np.array(applicant_prefers_input, dtype=int)
+     host_preferences = np.array(host_prefers_input, dtype=int)
+
+     # 選好表の行列数をチェック
+     row, col = applicant_preferences.shape
+     row_host, col_host = host_preferences.shape
+     if row != col_host or col != row_host:
+          exit(-1)
+
+     applicants_score = 0
+     hosts_score = 0
+
+     for applicant, host in matching.items():
+          applicant_preference = applicant_preferences[applicant]
+          host_preference = host_preferences[host]
+
+          rank_host = np.where(applicant_preference == host)[0][0]
+          rank_applicant = np.where(host_preference == applicant)[0][0]
+
+          applicants_score += (col-1) - rank_host
+          hosts_score += (row-1) - rank_applicant
+
+     return [applicants_score, hosts_score]
+
+
+
+
+for i in range(10):
      print("\n+-------------------------------------+\n")
      print("{0}回目".format(i))
      app_table = random_preference_table(5, 5)
      hos_table = random_preference_table(5, 5)
      matching = gale_shapley(app_table, hos_table)
      is_stable = is_stable_matching(matching, app_table, hos_table)
+     app_score, hos_score = matching_score(matching, app_table, hos_table)
+     all_match = all_stable_matching(app_table, hos_table)
+     score_match = []
+
+     for m in all_match:
+          score_match.append(matching_score(m, app_table, hos_table))
 
      print("申し込み側の選好は: {0}".format(app_table))
      print("受け入れ側の選好は: {0}".format(hos_table))
      print("マッチングは: {0}".format(matching))
      print("安定マッチングが得られたか? {0}".format(is_stable))
+     print("マッチングのスコアは: 申し込み側 {0}ポイント, 受け入れ側 {1}ポイント".format(app_score, hos_score))
+
+     print("\n全マッチングとスコアは、")
+     for i in range(len(score_match)):
+          print(all_match[i], ": ", score_match[i])
+
+     print("です。")
+
+
+
+"""
+print(matching_score(
+     {0: 4, 1: 1, 2: 3, 3: 2, 4: 0},
+     [[2, 4, 3, 1, 0], [1, 0, 3, 4, 2], [1, 3, 2, 0, 4], [2, 4, 1, 3, 0], [4, 0, 2, 3, 1]],
+     [[0, 2, 1, 3, 4], [1, 4, 3, 0, 2], [4, 3, 2, 0, 1], [4, 2, 1, 0, 3], [1, 0, 3, 4, 2]]
+     ))
+"""
+
+"""
+print(all_stable_matching(
+     [[2, 4, 3, 1, 0], [1, 0, 3, 4, 2], [1, 3, 2, 0, 4], [2, 4, 1, 3, 0], [4, 0, 2, 3, 1]],
+     [[0, 2, 1, 3, 4], [1, 4, 3, 0, 2], [4, 3, 2, 0, 1], [4, 2, 1, 0, 3], [1, 0, 3, 4, 2]]
+     ))
+"""
+
+
 
 
 
