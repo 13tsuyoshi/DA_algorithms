b6b4700b1731aebf448027c1bea056c5bb51439f
myuuuuun <mail@myuuuuun.com>
2015-06-20 03:17:37 +0900

    リスト内包にしたりジェネレータを使ったりしてみたものの、ほとんど速くならなかったので不要な部分を削除

--- speed_test/da_algorithm_speedtest.py
+++ speed_test/da_algorithm_speedtest.py
@@ -7,7 +7,6 @@ import math
 from random import shuffle
 import numpy as np
 import time
-from numba.decorators import jit
 np.set_printoptions(threshold=np.nan)
 
 
@@ -219,112 +218,6 @@ def gale_shapley2(applicant_prefers_input, host_prefers_input, **kwargs):
      return applicant_matchings, host_matchings
 
 
-# for文を消す
-def gale_shapley3(applicant_prefers_input, host_prefers_input, **kwargs):
-     start1 = time.time()
-     unmatch = kwargs.get('unmatch', True)
-
-     start3 = time.time()
-     # 選好表の型チェック
-     # listならnumpyへ変換
-     if isinstance(applicant_prefers_input, list):
-          applicant_preferences = np.array(applicant_prefers_input, dtype=int)
-
-     elif isinstance(applicant_prefers_input, np.ndarray):
-          applicant_preferences = applicant_prefers_input
-
-     else:
-          print("入力はlist型かnumpy.ndarray型にしてください")
-          return False
-
-     if isinstance(host_prefers_input, list):
-          host_preferences = np.array(host_prefers_input, dtype=int)
-
-     elif isinstance(host_prefers_input, np.ndarray):
-          host_preferences = host_prefers_input
-
-     else:
-          print("入力はlist型かnumpy.ndarray型にしてください")
-          return False
-
-     # unmatchマークが入力されていない時は、選好表の列の最後にunmatch列をつくる
-     if not unmatch:
-          dummy_host = np.array(np.arange(app_row, 1))
-          applicant_preferences = np.c_[applicant_preferences, dummy_host]
-          dummy_applicant = np.array(np.arange(hos_row, 1))
-          host_preferences = np.c_[host_preferences, dummy_applicant]
-
-
-     # 選好表の行列数をチェック
-     app_row, app_col = applicant_preferences.shape
-     host_row, host_col = host_preferences.shape
-
-     if (app_row != host_col-1) or (host_row != app_col-1):
-          print("ERROR")
-          return False
-
-     applicant_unmatched_mark = app_col-1
-     host_unmatched_mark = host_col-1
-
-     stop3 = time.time() - start3
-     print("変換は " + str(stop3) + " 秒でした")
-
-     # ソート
-     start4 = time.time()
-     host_preferences = np.argsort(host_preferences, axis=-1)
-     stop4 = time.time() - start4
-     print("ソートは " + str(stop4) + " 秒でした")
-
-     # 未マッチング者のリスト
-     stack = list(range(app_row))
-
-     # マッチングを入れる（初期値は未マッチングflag）
-     applicant_matchings = np.zeros(app_row, dtype=int) + applicant_unmatched_mark
-     host_matchings = np.zeros(host_row, dtype=int) + host_unmatched_mark
-
-     # メインループ
-     start2 = time.time()
-     next_start = np.zeros(app_row, dtype=int)
-     while len(stack) > 0:
-          # スタックから1人応募者を取り出す
-          applicant = stack.pop()
-
-          # 取り出した応募者の選好表
-          applicant_preference = applicant_preferences[applicant]
-
-          # 選好表の上から順番にプロポーズ
-          for index, host in enumerate(applicant_preference[next_start[applicant]:]):
-               #print(applicant, host)
-               # unmatched_markまでapplicantがマッチングできなければ、アンマッチ
-               if host == applicant_unmatched_mark:
-                    break
-
-               # プロポーズする相手の選好表
-               host_preference = host_preferences[host]
-
-               # 相手の選好表で、応募者と現在のマッチング相手のどちらが順位が高いのか比較する
-               rank_applicant = host_preference[applicant]
-               matched = host_matchings[host]
-               rank_matched = host_preference[matched]
-               
-               # もし受け入れ側が新しい応募者の方を好むなら
-               if rank_matched > rank_applicant:
-                    applicant_matchings[applicant] = host
-                    host_matchings[host] = applicant
-
-                    # 既にマッチしていた相手がダミーでなければ、マッチングを解除する
-                    if matched != host_unmatched_mark:
-                         applicant_matchings[matched] = applicant_unmatched_mark
-                         stack.append(matched)
-
-                    next_start[applicant] = index
-                    break
-
-     stop2 = time.time() - start2
-     print("LOOPは " + str(stop2) + " 秒でした")
-     return applicant_matchings, host_matchings
-
-
 # 選好表をランダムに作る
 def random_preference_table(row, col, **kwargs):
      unmatch = kwargs.get('unmatch', True)
@@ -377,8 +270,8 @@ if __name__ == '__main__':
      
 
      start = time.time()
-     app_table = pseudo_random_preference_table(1000, 1000)
-     hos_table = pseudo_random_preference_table(1000, 1000)
+     app_table = pseudo_random_preference_table(2000, 2000)
+     hos_table = pseudo_random_preference_table(2000, 2000)
      stop = time.time() - start
      print("選好表生成は " + str(stop) + " 秒でした\n")
 
